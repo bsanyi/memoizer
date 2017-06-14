9f898e2e11ebe04ee5ed17413d0bd89f12e5b57a
Bedo Sandor <bedo.sandor@gmail.com>
2017-06-14 10:11:53 +0200

    on the way to hex.pm

--- mix.exs
+++ mix.exs
@@ -2,12 +2,26 @@ defmodule Memoizer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :memoizer,
-     version: "0.0.1",
-     elixir: "~> 1.2-rc",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :memoizer,
+      version: "0.1.0",
+      elixir: "~> 1.2",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+
+      description: "",
+      aliases: [],
+      package: package(),
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["bsanyi"],
+      licenses:    ["MIT"],
+      links:       %{"GitHub" => "https://github.com/bsanyi/memoizer"},
+    ]
   end
 
   # Configuration for the OTP application
