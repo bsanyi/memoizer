8584ac97e6cc4b1e9796b3a5fe162979e19a8f65
Bedo Sandor <bedo.sandor@gmail.com>
2017-06-14 10:15:37 +0200

    further steps towards Hex

--- mix.exs
+++ mix.exs
@@ -3,16 +3,18 @@ defmodule Memoizer.Mixfile do
 
   def project do
     [
-      app: :memoizer,
+      app:     :memoizer,
       version: "0.1.0",
-      elixir: "~> 1.2",
+      elixir:  "~> 1.2",
+
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
 
-      description: "",
-      aliases: [],
-      package: package(),
+      # Hex
+      description: "OTP app to memoize/cache expensive function calls",
+      aliases:     [],
+      package:     package(),
     ]
   end
 
@@ -24,22 +26,12 @@ defmodule Memoizer.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger],
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
